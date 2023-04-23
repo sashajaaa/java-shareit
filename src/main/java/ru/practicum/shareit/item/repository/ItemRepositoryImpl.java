9646e673e3596033047bb33ea31b5aeed5f0b19b
sashajaaa <sashajaaa@yandex.ru>
2023-04-23 13:25:31 +0300

    Commit №13.3: refactor: -переработан класс AlreadyExistsException; -переработан класс ItemRepositoryImpl; -переработан класс ItemService; -переработан класс UserRepositoryImpl; -переработан класс UserService.

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
