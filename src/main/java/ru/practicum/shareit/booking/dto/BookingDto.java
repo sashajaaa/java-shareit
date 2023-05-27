9fa1d819006c7695ea1d8950662f757f9c1032c6
sashajaaa <sashajaaa@yandex.ru>
2023-05-27 13:57:55 +0300

    Commit №15.1: feat: -добавлена пагинация; -добавлена реализация запросов и ответов (Item requests); -добавлены тесты.
    refactor:
    -переработан файл pom.xml;
    -переработан файл schema.sql;
    -переработан класс BookingController;
    -переработан класс BookingMapper;
    -переработан интерфейс BookingRepository;
    -добавлен интерфейс BookingService;
    -переработан класс BookingServiceImpl;
    -переработан класс CommentMapper;
    -переработан класс ItemController;
    -переработан класс ItemDto;
    -переработан класс ItemMapper;
    -переработан класс Item;
    -переработан класс ItemRepository;
    -добавлен интерфейс ItemService;
    -переработан класс ItemServiceImpl;
    -переработан класс ItemRequestController;
    -переработан класс ItemRequestDto;
    -добавлен класс ItemRequestMapper;
    -переработан класс ItemRequest;
    -добавлен интерфейс ItemRequestRepository;
    -добавлен интерфейс ItemRequestService;
    -добавлен класс ItemRequestServiceImpl;
    -переработан класс UserController;
    -переработан класс UserMapper;
    -переработан класс UserService;
    -добавлен интерфейс UserService;
    -переработан класс UserServiceImpl.

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder(toBuilder = true)
+@Data
+@AllArgsConstructor
+public class BookingDto {
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long itemId;
+    private Long bookerId;
+    private String status;
+}
